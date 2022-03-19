aaf148b755b4483137203f16fe6b4671f00192c9
or <skhn7412@gmail.com>
2022-03-19 18:41:59 +0200

    clear button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let btn = document.getElementById("btn");
+let clearBtn = document.getElementById("clear");
 
 btn.addEventListener("click",()=>{
     let r =document.getElementById("red").value;
@@ -6,4 +7,10 @@ btn.addEventListener("click",()=>{
     let b = document.getElementById("blue").value;
 
     document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+})
+
+clearBtn.addEventListener("click",()=>{
+  document.getElementById("red").value = "";
+  document.getElementById("green").value = "";
+  document.getElementById("blue").value = "";
 })
